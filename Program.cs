559f7c0461540513f74099ede10d271d2ce652ed
Tuyên Phạm <DT050236@actvn.edu.vn>
2023-12-31 02:37:43 +0700

    Add CORS middleware and update database connection string

--- Program.cs
+++ Program.cs
@@ -85,6 +85,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
 app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthentication();
